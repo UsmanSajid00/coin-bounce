3bb5cf3d69452faa4db1090c034b3d8058eaf611
usmansajid00 <usmansajid2517@gmail.com>
2024-04-02 21:58:10 +0500

    writting APIs for createblog

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,8 +1,32 @@
+import Joi from "joi";
+import fs from "fs";
+
+const mongodbIdPattern = /^[0-9a-fA-F]{24}$/;
 const blogController = {
-  async create() {},
-  async getAll() {},
-  async getById() {},
-  async update() {},
-  async delete() {},
+  async create(req, res, next) {
+    const createBlogSchema = Joi.object({
+      title: Joi.string().required(),
+      author: Joi.string().regex(mongodbIdPattern).required(),
+      content: Joi.string().required(),
+      photo: Joi.string().required(),
+    });
+    const { error } = createBlogSchema.validate(req.body);
+    if (error) {
+      return next(error);
+    }
+    const { title, author, content, photo } = req.body;
+    const buffer = Buffer.from(
+      photo.replaced(/^data:image\/(png|jpg|jpeg);base64,/, ""),
+      "base64"
+    );
+    const imagePath = `${Date.now()}-${author}`;
+    try {
+      fs.writeFileSync("storages/${imagePath}", buffer);
+    } catch (error) {}
+  },
+  async getAll(req, res, next) {},
+  async getById(req, res, next) {},
+  async update(req, res, next) {},
+  async delete(req, res, next) {},
 };
 export default blogController;
