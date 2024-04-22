766227f5d8a69d5ba341a8b6c2ae5f3548212a6c
usmansajid00 <usmansajid2517@gmail.com>
2024-04-22 23:58:20 +0500

    settig up global state for the app

--- client/src/store/userSlice.js
+++ client/src/store/userSlice.js
@@ -1,1 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const inistalState = {
+  _id: "",
+  email: "",
+  username: "",
+  auth: false,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: inistalState,
+  reducers: {
+    setUser: (state, action) => {
+      const { id, email, username, auth } = action.payload;
+    },
+    resetUser: (state, action) => {},
+  },
+});
+
+export const { setUser, resetUser } = userSlice.actions;
+
+export default userSlice.reducer;
